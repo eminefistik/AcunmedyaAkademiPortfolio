dfd475848a3e36ad7f5e68d58de58965a46e6b75
eminefistik <eminefistik2734@gmail.com>
2025-02-04 14:15:16 +0300

    .

---
+++ AcunmedyaAkademiPortfolio/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunmedyaAkademiPortfolio.Models;
+
+namespace AcunmedyaAkademiPortfolio.Controllers
+{
+    public class CategoryController : Controller
+    {
+        AcunmedyaAkademi1 db = new AcunmedyaAkademi1();
+        public ActionResult CategoryList()
+        {
+            var values = db.catagorytbl.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(catagorytbl category) 
+        {
+            db.catagorytbl.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+        public ActionResult DeleteCategory(int id) 
+        {
+            var value = db.catagorytbl.Find(id);
+            db.catagorytbl.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
+      
+        }
+        [HttpGet]
+        public ActionResult UpdateCategory(int id) 
+        {
+            var value = db.catagorytbl.Find(id);
+            return View(value);
+        
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(catagorytbl p) 
+        {
+            var value = db.catagorytbl.Find(p.categoryId);
+            value.catagoryName =p.catagoryName;
+            db.SaveChanges();
+            return RedirectToAction("CategoryList");
+
+
+        }
+
+    }
+}
---
+++ AcunmedyaAkademiPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunmedyaAkademiPortfolio.Models;
+
+
+namespace AcunmedyaAkademiPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        AcunmedyaAkademi1 db = new AcunmedyaAkademi1();
+
+
+        public PartialViewResult PartialProject()
+        {
+            var values = db.tblproject.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialServiced()
+        {
+            var values = db.TblServiced.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Partialtestimoniall()
+        {
+            var values = db.Tbltestimoniall.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialMessage()
+        {
+            var values = db.TblMessage.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialEducation()
+        {
+            var values = db.TblEducation.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialExperience()
+        {
+            var values = db.TblExperience.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialSkill()
+        {
+            var values = db.TblSkill.ToList();
+            return PartialView(values);
+        }
+       
+
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblAbout.ToList();
+
+            return PartialView(values);
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead() 
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialHeroSection()
+        {
+            return PartialView();
+        }
+    }
+}
